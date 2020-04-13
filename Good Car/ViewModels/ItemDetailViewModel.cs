4bd51688d97bf94d6715e072fa8748b1de4bc224
Vilius <vilius1998@gmail.com>
2020-04-13 22:59:56 +0300

    Add project files.

---
+++ Good Car/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Good_Car.Models;
+
+namespace Good_Car.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Good Car/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Good Car.ViewModels
+{
+	public class SettingsViewModel : ContentView
+{
+    public SettingsViewModel()
+    {
+        Content = new StackLayout
+        {
+            Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+        };
+    }
+}
+}
